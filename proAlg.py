e88bd995616ff034820adb0c54f3820418e08344
Rafaín Rodríguez San Millán <38697752+rafain@users.noreply.github.com>
2018-10-22 11:21:22 -0500

    Esqueleto para el proyecto

---
+++ proAlg.py
@@ -0,0 +1,44 @@
+# Proyecto de comparación de algoritmos de ordenamiento.
+# Realizado por:
+#   Jesús Perea
+#   Samuel Sánchez
+#   Juan Manuel Gómez
+#   Rafaín Rodríguez
+#
+# 13-nov-2018
+
+def quickSort(list):
+    comp = 0
+    inter = 0
+    despl = 0
+
+def selectionSort(list):
+    comp = 0
+    inter = 0
+    despl = 0
+
+def heapSort(list):
+    comp = 0
+    inter = 0
+    despl = 0
+
+def mergeSort(list):
+    comp = 0
+    inter = 0
+    despl = 0
+
+def bubbleSort(list):
+    comp = 0
+    inter = 0
+    despl = 0
+
+def insertionSort(list):
+    comp = 0
+    inter = 0
+    despl = 0
+
+
+def main():
+    
+
+main()
