db47a098fd9bdc7bee637600c44d24d7cebd9675
Rafaín Rodríguez San Millán <38697752+rafain@users.noreply.github.com>
2018-11-12 12:59:46 -0600

    Update proAlg.py

--- proAlg.py
+++ proAlg.py
@@ -11,7 +11,10 @@
 from __future__ import division
 from __future__ import print_function
 from random import randint
+import time
+aleatoria = 0
 
+#TERMINADO NO TOCAR
 def quickSort(lis, first, last):
   comp = 0
   inter = 0
@@ -27,6 +30,9 @@ def quickSort(lis, first, last):
     comp += comp2 + comp3 + comp4
     inter += inter2 + inter3 + inter4
     despl += despl2 + despl3 + despl4
+  
+  if (aleatoria == 1):
+    print(lis)
 
   return (comp, inter, despl)
 
@@ -43,10 +49,10 @@ def quickSortH(lis, first, last):
   while not Done:
     comp += 1
     while (left <= right and lis[left] <= pivot):
-      comp += 2
+      comp += 1
       left += 1
     while (left <= right and lis[right] >= pivot):
-      comp += 2
+      comp += 1
       right -= 1
     comp += 1
     if (left > right):
@@ -60,11 +66,12 @@ def quickSortH(lis, first, last):
   inter += 1
   lis[first] = lis[right]
   lis[right] = pivot
-
+  print(lis)
   return (comp, inter, despl, right)
+#Aqui termina QuickSort
 
 
-
+#TERMINADO NO TOCAR
 def selectionSort(lis):
   comp = 0
   inter = 0
@@ -81,56 +88,67 @@ def selectionSort(lis):
     prob = lis[max]
     lis[max] = lis[j]
     lis[j] = prob
+    if (aleatoria == 1):
+      print(lis) 
 
   return (comp, inter, despl)
+#Aqui termina selectionSort
 
 
-def parent(index):
-    return int((index-1)/2)
-
-def leftChild(index):
-    return (index*2)+1
-
-def rightChild(index):
-    return (index*2)+2
-
-def swap(arr, parent, child):
-    if child>parent:
-        temp=arr[parent]
-        arr[parent]=arr[child]
-        arr[child]=temp
-
-def heapify(arr, n, parent):                
-    toSwap = parent
- 
-    if leftChild(parent) < n and arr[toSwap] < arr[leftChild(parent)]:
-        toSwap = leftChild(parent)
- 
-    if rightChild(parent) < n and arr[toSwap] < arr[rightChild(parent)]:
-        toSwap = rightChild(parent)
- 
-    if toSwap != parent:
-        swap(arr,parent,toSwap)
-        heapify(arr, n, toSwap)
-        
-def heapSort(arr):
+#TERMINADO NO TOCAR
+def heapify(lis, n, parent): 
     comp = 0
-    inter = 0
-    despl = 0
-    for i in range(len(arr), -1, -1):
-      heapify(arr, len(arr), i)
-      
-    for i in range(len(arr)-1, 0, -1):
-      swap(arr,0,i)
-      heapify(arr, i, 0)
+    swaps = 0
+    swapping = parent
+    rightchild = 2 * parent +2
+    leftchild = 2 * parent +1
+    
+    if leftchild < n and lis[parent] < lis[leftchild]: 
+        swapping = leftchild 
+        comp += 1
+        
+    if rightchild < n and lis[swapping] < lis[rightchild]: 
+        swapping = rightchild 
+        comp += 1
+        
+    if swapping != parent: 
+        lis[parent],lis[swapping] = lis[swapping],lis[parent] 
+        heapify(lis, n, swapping)
+        swaps += 1
+        
+    return (comp, swaps)
+  
+def heapSort(lis): 
+    n = len(lis) 
+    comp = 0
+    swaps = 0
 
+    for i in range(n, -1, -1): 
+        comp2, swaps2 = (heapify(lis, n, i)) 
+        comp += comp2
+        swaps += swaps2
+
+    for i in range(n-1, 0, -1): 
+        lis[i], lis[0] = lis[0], lis[i]
+        comp3, swaps3 = (heapify(lis, i, 0))
+        comp += comp3
+        swaps += swaps3
+        if aleatoria == 0:
+            print(lis)
+    return(lis, comp, swaps)
+#Aquí termina HEAP SORT
+
+
+#TERMINADO NO TOCAR
 def mergeSort(lis):
   comp = 0
   inter = 0
   despl = 0
 
-  comp += 1
   if len(lis) <= 1:
+    if (aleatoria == 1):
+      print(lis)
+
     return (lis, comp, inter, despl)
   else:
     split = len(lis)//2
@@ -141,6 +159,8 @@ def mergeSort(lis):
     inter += inter2 + inter3 + inter4
     despl += despl2 + despl3 + despl4
 
+    if (aleatoria == 1):
+      print(lis)
     return (lis, comp, inter, despl)
 
 def merge(lis, lis2):
@@ -150,7 +170,6 @@ def merge(lis, lis2):
 
   res = []
   while(len(lis) > 0 and len(lis2) > 0):
-    comp += 2
 
     comp += 1
     if (lis[0] < lis2[0]):
@@ -165,9 +184,11 @@ def merge(lis, lis2):
     res += lis2
   
   return (res, comp, inter, despl)
+#Aqui termina Merge Sort
 
 
 
+#TERMINADO NO TOCAR
 def bubbleSort(lis):
   comp = 0
   inter = 0
@@ -182,11 +203,13 @@ def bubbleSort(lis):
         prob = lis[i+1]
         lis[i+1] = lis[i]
         lis[i] = prob
+    if (aleatoria == 1):
+      print(lis)
   return (comp, inter, despl)
+#Aquí termina bubbleSort
 
 
-
-
+#TERMINADO NO TOCAR
 def insertionSort(lis):
   comp = 0
   inter = 0
@@ -203,8 +226,11 @@ def insertionSort(lis):
       pos = pos-1
     
     lis[pos] = val
+    if (aleatoria == 1):
+      print(lis)
     
   return(comp,inter,despl)
+#Aquí termina insertionSort
 
 
 def llenarLista():
@@ -261,6 +287,46 @@ def main():
       except Exception:
         print("Introduzca una opción válida")
         aleatoria = 0
+
+  #Corrida para Bubble Sort
+  print("Bubble Sort: ")
+  lisBS = lista.copy()
+
+  start = time.time()
+  compBS, interBS, desplBS = bubbleSort(lisBS)
+  end = time.time()
+
+  print("")
+  print("**********************************************************")
+  if (aleatoria == 1):
+    print("Notación O de Comparaciones: ")
+    print("Notación O de Intercambios: ")
+    
+  print("Realizadas.  Comparaciones: "+ compBS+ " Intercambios: " + interBS + "Desplazamientos: " + desplBS)
+  print("Tiempo de ejecución" + end-start)
+
+
+
+   #Variables y lista para Selection Sort
+  lisSS = lista.copy()
+  compSS, interSS, desplSS = 0,0,0
+
+  #Variables y lista para Insertion Sort
+  lisIS = lista.copy()
+  compIS, interIS, desplIS = 0,0,0
+
+  #Variables y lista para Merge Sort
+  lisMS = lista.copy()
+  compMS, interMS, desplMS = 0,0,0
+
+  #Variables y lista para Heap Sort
+  lisHS = lista.copy()
+  compHS, interHS, desplHS = 0,0,0
+
+  #Variables y lista para Quick Sort
+  lisQS = lista.copy()
+  compQS, interQS, desplQS = 0,0,0
+
   #print(lista)
 
 main()
